eb7c899b8ecd46cfae3693b7a713b1e3cfc47f27
Sascha Moser <28950736+xopez@users.noreply.github.com>
2021-09-08 19:58:56 +0200

    Update fire_uptimerobot.sh

--- fire_uptimerobot.sh
+++ fire_uptimerobot.sh
@@ -8,30 +8,30 @@ response=$(curl --head --write-out '%{http_code}' --silent --output /dev/null ht
 
 if [ "$response" == "200" ]; then
 
-	curl https://uptimerobot.com/inc/files/ips/IPv4andIPv6.txt -o /tmp/uptimerobot_IPv4andIPv6.txt
-	
-	# remove \r
-	sed -i 's/\r$//' /tmp/uptimerobot_IPv4andIPv6.txt
-	
-	# split IPv4 and IPv6
-	< /tmp/uptimerobot_IPv4andIPv6.txt grep "\." > /tmp/uptimerobot_IPv4.txt
-	< /tmp/uptimerobot_IPv4andIPv6.txt grep "\:" > /tmp/uptimerobot_IPv6.txt
+    curl https://uptimerobot.com/inc/files/ips/IPv4andIPv6.txt -o /tmp/uptimerobot_IPv4andIPv6.txt
+    
+    # remove \r
+    sed -i 's/\r$//' /tmp/uptimerobot_IPv4andIPv6.txt
+    
+    # split IPv4 and IPv6
+    < /tmp/uptimerobot_IPv4andIPv6.txt grep "\." > /tmp/uptimerobot_IPv4.txt
+    < /tmp/uptimerobot_IPv4andIPv6.txt grep "\:" > /tmp/uptimerobot_IPv6.txt
 
-	# extend with CIDR
-	IPv4="\"$(sed ':a;N;$!ba;s/\n/\/32","/g' /tmp/uptimerobot_IPv4.txt)\/32\""
-	IPv6="\"$(sed ':a;N;$!ba;s/\n/\/128","/g' /tmp/uptimerobot_IPv6.txt)\/128\""
+    # extend with CIDR
+    IPv4="\"$(sed ':a;N;$!ba;s/\n/\/32","/g' /tmp/uptimerobot_IPv4.txt)\/32\""
+    IPv6="\"$(sed ':a;N;$!ba;s/\n/\/128","/g' /tmp/uptimerobot_IPv6.txt)\/128\""
 
-	curl \
-		-X PUT \
-		-H "Authorization: Bearer $API_TOKEN" \
-		-H "Content-Type: application/json" \
-		-d '{"name":"Uptimerobot '"$(date +%d.%m.%Y)"' '"$(date +%R)"'"}' \
-		'https://api.hetzner.cloud/v1/firewalls/'$FIREWALL_ID
+    curl \
+        -X PUT \
+        -H "Authorization: Bearer $API_TOKEN" \
+        -H "Content-Type: application/json" \
+        -d '{"name":"Uptimerobot '"$(date +%d.%m.%Y)"' '"$(date +%R)"'"}' \
+        'https://api.hetzner.cloud/v1/firewalls/'"$FIREWALL_ID"
 
-	curl \
-		-X POST \
-		-H "Authorization: Bearer $API_TOKEN" \
-		-H "Content-Type: application/json" \
-		-d '{"rules":[{"direction":"in","source_ips":['"$IPv4"'],"protocol":"icmp"},{"direction":"in","source_ips":['"$IPv6"'],"protocol":"icmp"}]}' \
-		'https://api.hetzner.cloud/v1/firewalls/'$FIREWALL_ID'/actions/set_rules'
+    curl \
+        -X POST \
+        -H "Authorization: Bearer $API_TOKEN" \
+        -H "Content-Type: application/json" \
+        -d '{"rules":[{"direction":"in","source_ips":['"$IPv4"'],"protocol":"icmp"},{"direction":"in","source_ips":['"$IPv6"'],"protocol":"icmp"}]}' \
+        'https://api.hetzner.cloud/v1/firewalls/'"$FIREWALL_ID"'/actions/set_rules'
 fi
