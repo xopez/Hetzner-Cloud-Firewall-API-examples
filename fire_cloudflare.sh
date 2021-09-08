0fefad2bea5395663f3563e430f824efc95863e1
Sascha Moser <28950736+xopez@users.noreply.github.com>
2021-09-08 19:59:39 +0200

    Update fire_cloudflare.sh

--- fire_cloudflare.sh
+++ fire_cloudflare.sh
@@ -10,30 +10,30 @@ responseipv6=$(curl --head --write-out '%{http_code}' --silent --output /dev/nul
 
 if [ "$responseipv4" == "200" ] && [ "$responseipv6" == "200" ]; then
 
-	curl https://www.cloudflare.com/ips-v4 -o /tmp/cf_ips-v4
-	curl https://www.cloudflare.com/ips-v6 -o /tmp/cf_ips-v6
+    curl https://www.cloudflare.com/ips-v4 -o /tmp/cf_ips-v4
+    curl https://www.cloudflare.com/ips-v6 -o /tmp/cf_ips-v6
 
-	IPv4="\"$(sed ':a;N;$!ba;s/\n/","/g' /tmp/cf_ips-v4)\""
-	IPv6="\"$(sed ':a;N;$!ba;s/\n/","/g' /tmp/cf_ips-v6)\""
-	RULES=()
+    IPv4="\"$(sed ':a;N;$!ba;s/\n/","/g' /tmp/cf_ips-v4)\""
+    IPv6="\"$(sed ':a;N;$!ba;s/\n/","/g' /tmp/cf_ips-v6)\""
+    RULES=()
 
-	for PORT in $(echo "${PORTS//,/ }"); do
-		for IPS in "$IPv4" "$IPv6"; do
-			RULES+=('{"direction":"in","source_ips":['"$IPS"'],"protocol":"tcp","port":"'"$PORT"'"}')
-		done
-	done
+    for PORT in $(echo "${PORTS//,/ }"); do
+        for IPS in "$IPv4" "$IPv6"; do
+            RULES+=('{"direction":"in","source_ips":['"$IPS"'],"protocol":"tcp","port":"'"$PORT"'"}')
+        done
+    done
 
-	curl \
-		-X PUT \
-		-H "Authorization: Bearer $API_TOKEN" \
-		-H "Content-Type: application/json" \
-		-d '{"name":"Cloudflare '"$(date +%d.%m.%Y)"' '"$(date +%R)"'"}' \
-		'https://api.hetzner.cloud/v1/firewalls/'"$FIREWALL_ID"
+    curl \
+        -X PUT \
+        -H "Authorization: Bearer $API_TOKEN" \
+        -H "Content-Type: application/json" \
+        -d '{"name":"Cloudflare '"$(date +%d.%m.%Y)"' '"$(date +%R)"'"}' \
+        'https://api.hetzner.cloud/v1/firewalls/'"$FIREWALL_ID"
 
-	curl \
-		-X POST \
-		-H "Authorization: Bearer $API_TOKEN" \
-		-H "Content-Type: application/json" \
-		-d '{"rules":['"$(IFS=, ; echo "${RULES[*]}")"']}' \
-		'https://api.hetzner.cloud/v1/firewalls/'"$FIREWALL_ID"'/actions/set_rules'
+    curl \
+        -X POST \
+        -H "Authorization: Bearer $API_TOKEN" \
+        -H "Content-Type: application/json" \
+        -d '{"rules":['"$(IFS=, ; echo "${RULES[*]}")"']}' \
+        'https://api.hetzner.cloud/v1/firewalls/'"$FIREWALL_ID"'/actions/set_rules'
 fi
