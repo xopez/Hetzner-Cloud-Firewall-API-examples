3cf98e3d164eb911e0c5b861e1389f5902141d75
Sascha Moser <28950736+xopez@users.noreply.github.com>
2021-09-08 19:42:36 +0200

    Reduce string trimming to the minimum
    Only the right side needs to be trimmed to include the CIDR notation

--- dynamic_ip_whitelist.sh
+++ dynamic_ip_whitelist.sh
@@ -9,31 +9,31 @@ IPv4=$(curl --silent https://myip4.softcreatr.com/ | jq .ip)
 IPv6=$(curl --silent https://myip6.softcreatr.com/ | jq .ip)
 
 if [ ! -z "$IPv4" ] && [ ! -z "$IPv6" ]; then
-        IPS=\""${IPv4:1:-1}"/32\",\""${IPv6:1:-1}"/128\"
+    IPS="${IPv4::-1}"/32\","${IPv6::-1}"/128\"
 elif [ ! -z "$IPv4" ]; then
-        IPS=\""${IPv4:1:-1}"/32\"
+    IPS="${IPv4::-1}"/32\"
 elif [ ! -z "$IPv6" ]; then
-        IPS=\""${IPv6:1:-1}"/128\"
+    IPS="${IPv6::-1}"/128\"
 else
-        echo "No IP address provided"
-        exit 0
+    echo "No IP address provided"
+    exit 0
 fi
 
 RULES=()
 for PORT in $(echo "${PORTS//,/ }"); do
-        RULES+=('{"direction":"in","source_ips":['"$IPS"'],"protocol":"tcp","port":"'"$PORT"'"}')
+    RULES+=('{"direction":"in","source_ips":['"$IPS"'],"protocol":"tcp","port":"'"$PORT"'"}')
 done
 
 curl \
-        -X PUT \
-        -H "Authorization: Bearer $API_TOKEN" \
-        -H "Content-Type: application/json" \
-        -d '{"name":"Dynamic IP Whitelist '"$(date +%d.%m.%Y)"' '"$(date +%R)"'"}' \
-        'https://api.hetzner.cloud/v1/firewalls/'$FIREWALL_ID
+    -X PUT \
+    -H "Authorization: Bearer $API_TOKEN" \
+    -H "Content-Type: application/json" \
+    -d '{"name":"Dynamic IP Whitelist '"$(date +%d.%m.%Y)"' '"$(date +%R)"'"}' \
+    'https://api.hetzner.cloud/v1/firewalls/'$FIREWALL_ID
 
 curl \
-        -X POST \
-        -H "Authorization: Bearer $API_TOKEN" \
-        -H "Content-Type: application/json" \
-        -d '{"rules":['"$(IFS=, ; echo "${RULES[*]}")"']}' \
-        'https://api.hetzner.cloud/v1/firewalls/'$FIREWALL_ID'/actions/set_rules'
+    -X POST \
+    -H "Authorization: Bearer $API_TOKEN" \
+    -H "Content-Type: application/json" \
+    -d '{"rules":['"$(IFS=, ; echo "${RULES[*]}")"']}' \
+    'https://api.hetzner.cloud/v1/firewalls/'$FIREWALL_ID'/actions/set_rules'
