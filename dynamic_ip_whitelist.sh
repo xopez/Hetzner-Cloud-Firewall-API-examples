43b741adaef8f034519b8f33c7aa7be9a31ae3d1
Sascha Moser <28950736+xopez@users.noreply.github.com>
2021-09-09 00:34:48 +0200

    Some upgrades
    - Check requirements - thanks to @SoftCreatR and taken from him
    - dynamicly calculate IPv6 with CIDR notation since every device has it's own public ipv6 and the prefix can vary
    - added some magic

--- dynamic_ip_whitelist.sh
+++ dynamic_ip_whitelist.sh
@@ -4,16 +4,40 @@ API_TOKEN=""
 FIREWALL_ID=""
 PORTS="22,23,24"
 
+########################
+# Check requirements   #
+# Thanks to SoftCreatR #
+########################
+command_exists() {
+  command -v "$@" >/dev/null 2>&1
+}
+
+required_packages="curl jq sipcalc"
+
+for package in $required_packages; do
+  if ! command_exists "$package"; then
+    echo "The package $package is missing. Please install it, before continuing."
+    exit 1
+  fi
+done
+
 # get IP addresses
 IPv4=$(curl --silent https://myip4.softcreatr.com/ | jq .ip)
 IPv6=$(curl --silent https://myip6.softcreatr.com/ | jq .ip)
 
+# Calc CIDR notation for IPv6
+if [ -n "$IPv6" ]; then
+	IPv6="${IPv6:1:-1}"
+	IPv6=$(sipcalc "$IPv6"/"$(ip addr show |grep "$IPv6"|awk 'FNR==1 { print $2}'| cut -d "/" -f 2)"|grep "Subnet prefix"|awk '{print $5}')
+fi
+
+# Some magic
 if [ -n "$IPv4" ] && [ -n "$IPv6" ]; then
-    IPS="${IPv4::-1}"/32\","${IPv6::-1}"/128\"
+    IPS="${IPv4::-1}"/32\",\""$IPv6"\"
 elif [ -n "$IPv4" ]; then
     IPS="${IPv4::-1}"/32\"
 elif [ -n "$IPv6" ]; then
-    IPS="${IPv6::-1}"/128\"
+    IPS=\""$IPv6"\"
 else
     echo "No IP address provided"
     exit 0
